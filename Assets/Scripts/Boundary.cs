4517b062fbdc43d54883ec9b2d95d8ca8772855f
SashaKhyzhun <sasha6427@gmail.com>
2016-03-16 15:07:11 +0200

    wallpaper is done, player anim 50% done, git ignore, +smth

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boundary : MonoBehaviour {
+
+    public Transform targetPlayer;
+    public Transform targetEnemy;
+
+    public float lastOffsetX;
+    public float lastOffsetY;
+
+    private Transform playerTransform;
+    private Transform enemyTransform;
+
+    private Renderer playerRenderer;
+    private Renderer enemyRenderer;
+
+    private float cameraExtentY;
+    private float cameraExtentX;
+
+    private float playerExtentY;
+    private float playerExtentX;
+
+    private float enemyExtentY;
+    private float enemyExtentX;
+
+    void Start () {
+        // for player
+        playerTransform = transform;
+        playerRenderer = targetPlayer.GetComponent<Renderer>();
+        // for enemy
+        enemyTransform = transform;
+        enemyRenderer = targetEnemy.GetComponent<Renderer>();
+        // for all
+        cameraExtentY = Camera.main.orthographicSize;
+        cameraExtentX = cameraExtentY * Camera.main.aspect;
+        //for player
+        playerExtentY = playerRenderer.bounds.extents.y;
+        playerExtentX = playerRenderer.bounds.extents.x;
+        //for enemy
+        enemyExtentY = enemyRenderer.bounds.extents.y;
+        enemyExtentX = enemyRenderer.bounds.extents.x;
+
+    }
+
+    void Update () {
+        //for player
+	    if (playerRenderer.bounds.center.x >= playerTransform.position.x + cameraExtentX + playerExtentX + lastOffsetX) {
+            targetPlayer.position += new Vector3(-(cameraExtentX + playerExtentX + lastOffsetX) * 2, 0, 0);
+        }
+        if (playerRenderer.bounds.center.x < playerTransform.position.x - cameraExtentX - playerExtentX - lastOffsetX) {
+            targetPlayer.position += new Vector3((cameraExtentX + playerExtentX + lastOffsetX) * 2, 0, 0);
+        }
+        if (playerRenderer.bounds.center.y >= playerTransform.position.y + cameraExtentY + playerExtentY + lastOffsetY) {
+            targetPlayer.position += new Vector3(0, -(cameraExtentY + playerExtentY + lastOffsetY) * 2, 0);
+        }
+        if (playerRenderer.bounds.center.y < playerTransform.position.y - cameraExtentY - playerExtentY - lastOffsetY) {
+            targetPlayer.position += new Vector3(0, (cameraExtentY + playerExtentY + lastOffsetY) * 2, 0);
+        }
+
+        //for enemy
+        if (enemyRenderer.bounds.center.x >= enemyTransform.position.x + cameraExtentX + enemyExtentX + lastOffsetX) {
+            targetEnemy.position += new Vector3(-(cameraExtentX + enemyExtentX + lastOffsetX) * 2, 0, 0);
+        }
+        if (enemyRenderer.bounds.center.x < enemyTransform.position.x - cameraExtentX - enemyExtentX - lastOffsetX) {
+            targetEnemy.position += new Vector3((cameraExtentX + enemyExtentX + lastOffsetX) * 2, 0, 0);
+        }
+        if (enemyRenderer.bounds.center.y >= enemyTransform.position.y + cameraExtentY + enemyExtentY + lastOffsetY) {
+            targetEnemy.position += new Vector3(0, -(cameraExtentY + enemyExtentY + lastOffsetY) * 2, 0);
+        }
+        if (enemyRenderer.bounds.center.y < enemyTransform.position.y - cameraExtentY - enemyExtentY - lastOffsetY) {
+            targetEnemy.position += new Vector3(0, (cameraExtentY + enemyExtentY + lastOffsetY) * 2, 0);
+        }
+
+    }
+
+
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float speed = 5f;
+    bool facingRight = true;
+
+    private Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+
+    void FixedUpdate () {
+
+        float move = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * speed, 0);
+        if (move > 0 && !facingRight) {
+            Flip();
+        } else if (move < 0 && facingRight) {
+            Flip();
+        }
+    
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
