5e0ade4d9af3f1c3e68e2b51d2c8fcb38eb7109d
Blind56 <petrinko6@gmail.com>
2016-08-15 22:29:38 +0300

    анімашки і іконка

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,11 +3,28 @@ using System.Collections;
 
 public class Ball : ProjectileController
 {
+    public float animationTime;
+
+    private Animator anim;
+    private Collider2D col;
+    private Rigidbody2D rb;
+    private WaitForSeconds animationTimewfs;
+    private string triggerName = "explode";
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        col = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        animationTimewfs = new WaitForSeconds(animationTime);
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        triggerName = "explode";
         if (collision.gameObject.CompareTag("Player"))
         {
+            triggerName = "explodeMidAir";
             scoreController.Wasted();
             collision.gameObject.GetComponent<PlayerController>().Die();
         }
@@ -16,8 +33,7 @@ public class Ball : ProjectileController
             scoreController.MeteorScoreUp();
         }
 
-        Kill();
-
+        StartCoroutine(KillCoroutine());
     }
 
     protected override void Kill()
@@ -26,6 +42,16 @@ public class Ball : ProjectileController
         gameObject.SetActive(false);
     }
 
+    IEnumerator KillCoroutine()
+    {
+        anim.SetTrigger(triggerName);
+        col.enabled = false;
+        rb.isKinematic = true;
+        yield return animationTimewfs;
+        col.enabled = true;
+        rb.isKinematic = false;
+        Kill();
+    }
 
 }
 
